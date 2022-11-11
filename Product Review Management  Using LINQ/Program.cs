a93b9b8ffb4b7f7f54a2c29db3be491a6279b5e9
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-11-11 12:35:44 +0530

    [Pratu]ADDED: welcome to Product Revire Management Using Linq

---
+++ Product Review Management  Using LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Product_Review_Management__Using_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Problems Using LINQ");
+        }
+    }
+}
